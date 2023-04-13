5a7e4e9002dd725c910f85f71015a63bd3166826
Tharahan Tharmaraja <tharahan04@hotmail.com>
2023-04-13 16:26:20 +0100

    questions up to 10

--- football_queries.sql
+++ football_queries.sql
@@ -22,3 +22,25 @@
 -- 4 teams
 
 -- 6) How many different teams have played in matches recorded in a French division?
+-- SELECT code FROM divisions WHERE country = 'France';    
+-- SELECT COUNT(DISTINCT hometeam) FROM matches WHERE division_code IN ('F1', 'F2');
+-- 61
+
+-- 7) Have Huddersfield played Swansea in any of the recorded matches?
+-- SELECT * FROM matches WHERE hometeam='Huddersfield' AND awayteam='Swansea' OR hometeam='Swansea' AND awayteam='Huddersfield';
+-- 12 times
+
+-- 8) How many draws were there in the `Eredivisie` between 2010 and 2015?
+-- SELECT code FROM divisions WHERE name = 'Eredivisie';
+-- SELECT COUNT(*) FROM matches WHERE division_code = 'N1' AND ftr = 'D' AND season BETWEEN 2010 AND 2015;
+
+-- 9) Select the matches played in the Premier League in order of total goals scored from highest to lowest. 
+-- When two matches have the same total the match with more home goals should come first.
+-- SELECT code FROM divisions WHERE name= 'Premier League';
+-- SELECT * FROM matches WHERE division_code = 'E0' ORDER BY (ftag + fthg, fthg)DESC;
+
+-- 10) In which division and which season were the most goals scored?
+SELECT division_code, season, SUM(fthg + ftag) FROM matches GROUP BY division_code, season ORDER BY sum DESC LIMIT 1;
+SELECT name FROM divisions WHERE code = 'EC';
+
+
